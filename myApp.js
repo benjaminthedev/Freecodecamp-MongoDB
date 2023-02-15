089b34c04b207502ceca3df498c67da87a933264
Benjamin Dordoigne <benjamindordoigne@benjamins-air.home>
2023-02-15 20:13:10 +0000

    updated

--- myApp.js
+++ myApp.js
@@ -1,7 +1,20 @@
 require('dotenv').config();
 
+const mongoose = require('mongoose');
 
-let Person;
+mongoose.connect(process.env.MONGO_URI);
+
+const Schema = mongoose.Schema;
+
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
+
+const Person = mongoose.model("Person", personSchema);
+// let Person;
+
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
